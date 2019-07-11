65bf4744a3d458973ce37a5a98955746a0eb6b93
Willem Leitso <willem@leit.so>
2019-07-11 15:04:27 +0200

    Canvas aligned proprely

--- assets/js/index.js
+++ assets/js/index.js
@@ -10,7 +10,7 @@ var enemies = [
 ]
 
 var components = []
-var canvdim = [1600, 900]
+var canvdim = [720, 480]
 
 var myGameArea = {
   canvas: document.getElementById('myCanvas'),
@@ -29,28 +29,38 @@ var myGameArea = {
 }
 
 function Component(width, height, color, x, y) {
-  this.width = width
-  this.height = height
-  this.x = x
-  this.y = y
   this.update = function() {
     var ctx
     ctx = myGameArea.context
     ctx.fillStyle = color
-    ctx.fillRect(this.x, this.y, this.width, this.height)
+    ctx.fillRect(x, y, width, height)
+  }
+}
+
+function Text(font, text, color, x, y, center) {
+  this.update = function() {
+    var ctx
+    ctx = myGameArea.context
+    ctx.font = font
+    if (center == true) {
+      ctx.textAlign = 'center'
+    } else {
+      ctx.textAlign = 'start'
+    }
+    ctx.fillText(text, x, y)
   }
 }
 
 // characters[i] = [name, basehealth, health, baseattack, attackone, attacktwo, attackunlim, imagename, posx, posy, attackthree (optional), attackfour (optional), strengthened]
 // attack* = [name, damage]
 
-characters[0] = ['Hammer Guy', 200, 200, 'Sword', '10', 'Hammer Smash', '50', 'Lightning', '100', 'Slam Drop', '30', 'hammer-guy.png', 20, 20, false]
-characters[1] = ['Magic Girl', 100, 100, 'Knife', '10', 'Icicle Launch', '40', 'Freeze', '70', 'Flash Blind', '20', 'magic-girl.png', 20, 70, false]
-characters[2] = ['Heal Dude', 150, 150, 'Scalpel', '10', 'Organ Removal', '30', 'Scalpel Rain', '50', 'Scissor Attack', '20', 'heal-dude.png', 20, 120, false, 'Heal', '50', 'Give Strength', '50']
+characters[0] = ['Hammer Guy', 200, 200, 'Sword', '10', 'Hammer Smash', '50', 'Lightning', '100', 'Slam Drop', '30', 'hammer-guy.png', 30, 50, false]
+characters[1] = ['Magic Girl', 100, 100, 'Knife', '10', 'Icicle Launch', '40', 'Freeze', '70', 'Flash Blind', '20', 'magic-girl.png', 30, 150, false]
+characters[2] = ['Heal Dude', 150, 150, 'Scalpel', '10', 'Organ Removal', '30', 'Scalpel Rain', '50', 'Scissor Attack', '20', 'heal-dude.png', 30, 250, false, 'Heal', '50', 'Give Strength', '50']
 
-enemies[0] = ['Troll', 200, 200, 'Club', '10', 'Boulder Throw', '50', 'Ground Smash', '100', 'ogre.png', 110, 20]
-enemies[1] = ['Goblin', 100, 100, 'Sword', '10', 'Fire Ball', '40', 'Burn', '70', 'goblin.png', 110, 70]
-enemies[2] = ['Elf', 150, 150, 'Spear', '10', 'Bow', '30', 'Snare', '50', 'elf.png', 110, 120]
+enemies[0] = ['Troll', 200, 200, 'Club', '10', 'Boulder Throw', '50', 'Ground Smash', '100', 'ogre.png', 300, 50]
+enemies[1] = ['Goblin', 100, 100, 'Sword', '10', 'Fire Ball', '40', 'Burn', '70', 'goblin.png', 300, 150]
+enemies[2] = ['Elf', 150, 150, 'Spear', '10', 'Bow', '30', 'Snare', '50', 'elf.png', 300, 250]
 
 function updateGameArea() {
   myGameArea.clear()
@@ -61,49 +71,50 @@ function updateGameArea() {
 }
 
 function drawHealthBarArea(x, y) {
-  var bottomy = y + 6
-  var rightx = x + 31
-  components.push(new Component(32, 1, 'black', x, y))
-  components.push(new Component(1, 6, 'black', x, y))
-  components.push(new Component(32, 1, 'black', x, bottomy))
-  components.push(new Component(1, 6, 'black', rightx, y))
+  var healthbarareawidth = 62
+  var healthbarareaheight = 9
+  var bottomy = y + healthbarareaheight
+  var rightx = x + healthbarareawidth - 1
+  components.push(new Component(healthbarareawidth, 1, 'black', x, y))
+  components.push(new Component(1, healthbarareaheight, 'black', x, y))
+  components.push(new Component(healthbarareawidth, 1, 'black', x, bottomy))
+  components.push(new Component(1, healthbarareaheight, 'black', rightx, y))
 }
 
 function drawHealthBar(x, y, basehealth, health) {
+  var healthbarwidth = 60
+  var healthbarheight = 8
   healthpercent = (health / basehealth) * 100
   if (healthpercent > 74) {
-    components.push(new Component((healthpercent / 100) * 30, 5, 'green', x + 1, y + 1))
+    components.push(new Component((healthpercent / 100) * healthbarwidth, healthbarheight, 'green', x + 1, y + 1))
   } else if (healthpercent > 24) {
-    components.push(new Component((healthpercent / 100) * 30, 5, 'orange', x + 1, y + 1))
+    components.push(new Component((healthpercent / 100) * healthbarwidth, healthbarheight, 'orange', x + 1, y + 1))
   } else if (healthpercent < 25) {
-    components.push(new Component((healthpercent / 100) * 30, 5, 'red', x + 1, y + 1))
+    components.push(new Component((healthpercent / 100) * healthbarwidth, healthbarheight, 'red', x + 1, y + 1))
   }
 }
 
 function drawChars() {
   components = []
+  var displacement = [160, 50] // [x, y]
   for (var i = 0; i < characters.length; i++) {
-    components.push(new Component(20, 20, 'green', characters[i][12], characters[i][13]))
-    drawHealthBarArea(characters[i][12] - 6, characters[i][13] - 10)
-    drawHealthBar(characters[i][12] - 6, characters[i][13] - 10, characters[i][1], characters[i][2])
+    components.push(new Component(50, 50, 'green', characters[i][12] + displacement[0], characters[i][13] + displacement[1]))
+    drawHealthBarArea(characters[i][12] - 6 + displacement[0], characters[i][13] - 14 + displacement[1])
+    drawHealthBar(characters[i][12] - 6 + displacement[0], characters[i][13] - 14 + displacement[1], characters[i][1], characters[i][2])
   }
   for (var i = 0; i < enemies.length; i++) {
-    components.push(new Component(20, 20, 'red', enemies[i][10], enemies[i][11]))
-    drawHealthBarArea(enemies[i][10] - 6, enemies[i][11] - 10)
-    drawHealthBar(enemies[i][10] - 6, enemies[i][11] - 10, enemies[i][1], enemies[i][2])
+    components.push(new Component(50, 50, 'red', enemies[i][10] + displacement[0], enemies[i][11] + displacement[1]))
+    drawHealthBarArea(enemies[i][10] - 6 + displacement[0], enemies[i][11] - 14 + displacement[1])
+    drawHealthBar(enemies[i][10] - 6 + displacement[0], enemies[i][11] - 14 + displacement[1], enemies[i][1], enemies[i][2])
   }
   myGameArea.start()
 }
 drawChars()
 
-function killChar(chartokill) {
-
-}
-
 function checkHealth() {
   for (var i = 0; i < characters.length; i++) {
     if (characters[i][2] < 1) {
-      killChar(characters[i])
+      characters[i][2] = 0
     } else {
       if (characters[i][2] > characters[i][1]) {
         characters[i][2] = characters[i][1]
@@ -112,7 +123,7 @@ function checkHealth() {
   }
   for (var i = 0; i < enemies.length; i++) {
     if (enemies[i][2] < 1) {
-      killChar(enemies[i])
+      enemies[i][2] = 0
     } else {
       if (enemies[i][2] > enemies[i][1]) {
         enemies[i][2] = enemies[i][1]
@@ -170,4 +181,8 @@ function support(supportnum, target) {
     target[13] == true
   }
   checkHealth()
+}
+
+function enemyAttack() {
+  Math.floor((Math.random() * 3))
 }
